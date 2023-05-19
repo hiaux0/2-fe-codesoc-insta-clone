b8e3382a380d523582f4e6a432b61fe968593420
hiaux0 <30693990+hiaux0@users.noreply.github.com>
2023-05-19 15:51:36 +0200

    Initial commit

---
+++ src/messages.ts
@@ -0,0 +1,26 @@
+// @ts-check
+
+/** @const */
+export const MSG = {
+  auth: {
+    login: "login"
+  },
+  connection: {
+    connection: "connection",
+    disconnect: "disconnect",
+    "connection created": "connection created"
+  },
+  message: {
+    "new message": "new message",
+    typing: "typing",
+    "stop typing": "stop typing"
+  },
+  user: {
+    "add user": "add user",
+    "change username": "change username",
+    updateUserList: "updateUserList",
+    "user already exists": "user already exists",
+    "user joined": "user joined",
+    "user left": "user left"
+  }
+};
